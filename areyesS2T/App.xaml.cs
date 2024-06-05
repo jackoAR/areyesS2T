ab302f6e07edcaf8a226a50af73b382367307662
jhsja <jhsja@ALEXPC>
2024-06-04 21:50:31 -0500

    Add project files.

---
+++ areyesS2T/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace areyesS2T
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ areyesS2T/Calificacion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace areyesS2T
+{
+    public class Calificacion
+
+
+    {
+
+        double seguimiento1;
+        double seguimiento2;
+
+        public static double Notaseguimiento1(double seguimiento1)
+        {
+            return seguimiento1 * 0.3;
+        }
+
+        public static double NotaExamen(double seguimiento2)
+        {
+            return seguimiento2 * 0.2;
+        }
+
+        public static double NotaParcial1(double seguimiento1, double seguimiento2)
+        {
+            return seguimiento1 + seguimiento2;
+        }
+
+        public static double NotaSeguimiento2(double seguimiento1P2)
+        {
+            return seguimiento1P2 * 0.2;
+        }
+
+        public static double NotaExamen2(double notaExamen2)
+        {
+            return notaExamen2 * 0.2;
+        }
+
+        public static double NotaFinal(double notaParcial1, double notaParcial2) {
+
+            return notaParcial1 + notaParcial2;
+        }
+
+        public static string Estado(double notaFinal)
+        {
+
+            if (notaFinal >= 7)
+            {
+                return "Aprobado";
+            }
+            else if (notaFinal >= 5 && notaFinal <= 6.9)
+            {
+                return "Complementario";
+            }
+            else if (notaFinal < 5)
+            {
+                return "Reprobado";
+            }
+                    
+             
+        }
+    }
+}
